5a3577a84f7525eb6d7bfbb0f8fe7db32c00ac73
Nikita Kuznetsov <nkuz1509@gmail.com>
2025-01-10 14:21:39 +0300

    Refactore @classmethod update_balance using with_for_update

--- app/wallets/repo.py
+++ app/wallets/repo.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, Request
 from fastapi.responses import JSONResponse
-from sqlalchemy import select
+from sqlalchemy import select, update
+from sqlalchemy.exc import IntegrityError
 
 from base_repo import BaseRepo
 from database import async_session_maker
@@ -17,28 +18,68 @@ class WalletRepo(BaseRepo):
             result = await session.execute(query)
             return result.scalars().first()
 
+    # @classmethod
+    # async def update_balance(cls, wallet_uuid: str, amount: float, operation_type: str):
+    #     async with async_session_maker() as session:
+    #         try:
+    #             wallet = await cls.get_wallet(wallet_uuid)
+    #             if not wallet:
+    #                 raise HTTPException(status_code=404, detail="Wallet not found")
+    #
+    #             if operation_type == 'DEPOSIT':
+    #                 new_balance = float(wallet.balance) + amount
+    #             elif operation_type == 'WITHDRAW':
+    #                 new_balance = float(wallet.balance) - amount
+    #             else:
+    #                 raise HTTPException(status_code=404, detail="Invalid operation_type")
+    #
+    #             if new_balance < 0:
+    #                 raise HTTPException(status_code=400, detail="Insufficient funds")
+    #
+    #             wallet.balance = new_balance
+    #             session.add(wallet)
+    #             await session.commit()
+    #             return {"message": f"{operation_type} successful"}
+    #         except Exception as e:
+    #             return JSONResponse(content={"error": str(e)}, status_code=500)
+
     @classmethod
     async def update_balance(cls, wallet_uuid: str, amount: float, operation_type: str):
         async with async_session_maker() as session:
             try:
-                wallet = await cls.get_wallet(wallet_uuid)
-                if not wallet:
-                    raise HTTPException(status_code=404, detail="Wallet not found")
+                async with session.begin():
+                    query = (
+                        select(Wallet)
+                        .filter(Wallet.uuid == wallet_uuid)
+                        .with_for_update(nowait=True)
+                    )
+                    result = await session.execute(query)
+                    wallet = result.scalar_one_or_none()
 
-                if operation_type == 'DEPOSIT':
-                    new_balance = float(wallet.balance) + amount
-                elif operation_type == 'WITHDRAW':
-                    new_balance = float(wallet.balance) - amount
-                else:
-                    raise HTTPException(status_code=404, detail="Invalid operation_type")
+                    if not wallet:
+                        raise HTTPException(status_code=404, detail="Wallet not found")
 
-                if new_balance < 0:
-                    raise HTTPException(status_code=400, detail="Insufficient funds")
+                    if operation_type == 'DEPOSIT':
+                        new_balance = float(wallet.balance) + amount
+                    elif operation_type == 'WITHDRAW':
+                        new_balance = float(wallet.balance) - amount
+                    else:
+                        raise HTTPException(status_code=404, detail="Invalid operation_type")
 
-                wallet.balance = new_balance
-                session.add(wallet)
-                await session.commit()
-                return {"message": f"{operation_type} successful"}
+                    if new_balance < 0:
+                        raise HTTPException(status_code=400, detail="Insufficient funds")
+
+                    stmt = (
+                        update(Wallet)
+                        .where(Wallet.uuid == wallet_uuid)
+                        .values(balance=new_balance)
+                    )
+                    await session.execute(stmt)
+
+                    await session.commit()
+                    return {"message": f"{operation_type} successful"}
+            except IntegrityError as e:
+                return JSONResponse(content={"error": str(e)}, status_code=500)
             except Exception as e:
                 return JSONResponse(content={"error": str(e)}, status_code=500)
 
